26e4ebd7b08302e53bd3198d5b354085e88b3f15
Wilsley Germano <74660388+wilsleygermano@users.noreply.github.com>
2022-04-28 14:38:47 -0300

    Update useless_app.dart

--- lib/classes/useless_app.dart
+++ lib/classes/useless_app.dart
@@ -3,16 +3,10 @@ import '../screens/my_home_page.dart';
 
 class UselessApp extends StatelessWidget {
   const UselessApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
       home: MyHomePage(),
     );
   }
