cb53bff9ef5236c7461087f0b9be91295495e5f2
Wilsley Germano <74660388+wilsleygermano@users.noreply.github.com>
2022-05-02 09:20:42 -0300

    Update fact_api_url.dart

--- lib/controller/fact_api_url.dart
+++ lib/controller/fact_api_url.dart
@@ -1,5 +1,5 @@
 factApiUrl(int randomNumber) {
-  String url = "https://picsum.photos/600?random=$randomNumber";
+  String url = "https://picsum.photos/2048?random=$randomNumber";
   return url;
 
 }
